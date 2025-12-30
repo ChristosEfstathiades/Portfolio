5e8bcd10e96894441f0ef06c78b41448f42855ff
d86617ce <chrise3012@icloud.com>
2025-12-30 01:07:50 +0000

    Genius SEO to rank me for the niche keyword known as 'Chris'

--- src/App.tsx
+++ src/App.tsx
@@ -59,8 +59,9 @@ function App() {
                             alt="My Face"
                         />
                         <p className="text-[#737373]">
-                            I'm an <b>Embedded Software Engineer at IQHQ</b> and
-                            a Computer Science graduate from the University of
+                            Hi I'm Chris, an{" "}
+                            <b>Embedded Software Engineer at IQHQ</b> and a
+                            Computer Science graduate from the University of
                             Manchester. I've built full-stack web applications
                             and enjoy tackling problems across the wider field
                             of Software Engineering, including data science,
