ab529bf9789d5e729d56f1fbe067348d4c1fcad0
Christos Efstathiades <46204113+ChristosEfstathiades@users.noreply.github.com>
2026-05-20 15:03:52 +0100

    Update transition classes for project links
    Fixes links from having transition animation when user enables dark mode / light mode

--- src/components/project-links.tsx
+++ src/components/project-links.tsx
@@ -40,7 +40,7 @@ export default function ProjectLinks({
                 href={github}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center transition px-3 hover:bg-accent font-semibold py-1 gap-1 bg-white dark:bg-[#111] shadow rounded"
+                className="flex items-center transition duration-0 hover:duration-150 px-3 hover:bg-accent font-semibold py-1 gap-1 bg-white dark:bg-[#111] shadow rounded"
             >
                 <img
                     className="h-5"
@@ -56,7 +56,7 @@ export default function ProjectLinks({
                     href={site}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center transition px-3 hover:bg-accent font-semibold py-1 gap-1 bg-white dark:bg-[#111] shadow rounded"
+                    className="flex items-center transition duration-0 hover:duration-150 px-3 hover:bg-accent font-semibold py-1 gap-1 bg-white dark:bg-[#111] shadow rounded"
                 >
                     <ExternalLink size={20} />
                     <p>Site</p>
