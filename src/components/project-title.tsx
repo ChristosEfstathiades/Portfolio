40ee32ae671c46f12fcf33b1375b357f61c1d48b
Christos Efstathiades <46204113+ChristosEfstathiades@users.noreply.github.com>
2026-07-07 12:13:33 +0100

    Rename ProjectTitle component to Project

--- src/components/project-title.tsx
+++ src/components/project-title.tsx
@@ -1,3 +1,7 @@
-export default function ProjectTitle({ title }: { title: string }) {
-    return <h3 className="text-[22px] font-bold tracking-tight mb-4">{title}</h3>;
+export default function Project({ children }: { children: React.ReactNode }) {
+    return (
+        <section className="max-w-3xl mb-6 mx-auto rounded-2xl flex-col flex dark:bg-[#202020] bg-white p-7 border border-[#ececea] dark:border-[#2c2c2c] shadow-sm dark:shadow-none">
+            {children}
+        </section>
+    );
 }
