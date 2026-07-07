8886685d1df0e237d01225cdea1c4cb601c8e741
Christos Efstathiades <46204113+ChristosEfstathiades@users.noreply.github.com>
2026-07-07 12:15:07 +0100

    Change ToolsTitle heading size and style

--- src/components/tools-title.tsx
+++ src/components/tools-title.tsx
@@ -1,6 +1,6 @@
 export default function ToolsTitle() {
     return (
-        <h4 className="text-lg pl-2.5 border-l-3 font-bold border-orange-primary">
+        <h4 className="text-xs uppercase tracking-wider pl-2.5 border-l-3 font-bold border-orange-primary">
             Tools
         </h4>
     );
