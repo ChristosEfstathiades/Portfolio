c63a0bbd5bc39e2fa0ab55f6c89300be7a7ce802
Christos Efstathiades <46204113+ChristosEfstathiades@users.noreply.github.com>
2026-07-07 12:14:29 +0100

    Update section styles in Project component

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,6 +1,6 @@
 export default function Project({ children }: { children: React.ReactNode }) {
     return (
-        <section className="max-w-3xl mb-5 mx-auto rounded-lg flex-col flex dark:bg-[#181818] bg-gray-100 p-6 border-black/5 border">
+        <section className="max-w-3xl mb-6 mx-auto rounded-2xl flex-col flex dark:bg-[#202020] bg-white p-7 border border-[#ececea] dark:border-[#2c2c2c] shadow-sm dark:shadow-none">
             {children}
         </section>
     );
