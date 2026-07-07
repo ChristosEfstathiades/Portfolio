169bc1c2460b5700cc823c7f9a9f0bcf0647fa09
Christos Efstathiades <46204113+ChristosEfstathiades@users.noreply.github.com>
2026-07-07 12:14:50 +0100

    Update Tool component styling and structure

--- src/components/tool.tsx
+++ src/components/tool.tsx
@@ -1,6 +1,6 @@
 export default function Tool({ name, image }: { name: string; image: string }) {
     return (
-        <div className="px-2 font-semibold py-0.5 gap-1 bg-white dark:bg-[#111] shadow flex rounded items-center">
+        <div className="px-[11px] font-medium py-[5px] gap-1.5 text-sm bg-[#f6f6f4] dark:bg-[#161616] border border-[#e7e7e4] dark:border-[#2c2c2c] flex rounded-lg items-center">
             <img className="h-5" src={image} alt="Logo" />
             <p>{name}</p>
         </div>
